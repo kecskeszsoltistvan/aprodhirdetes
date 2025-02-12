4949f1dfe701089c248ea2e5f2d60a669ccc8471
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2025-02-12 10:22:15 +0100

    Csinosítás

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -15,7 +15,7 @@ export const routes: Routes = [
     path: 'register', component: RegisterComponent
   },
   {
-    path: '*', component: HomeComponent
+    path: '**', component: HomeComponent
   },
   
 ];
