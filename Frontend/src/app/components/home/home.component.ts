e677c9b609dc692a11e34fee0eccb40d3c19d1e8
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2025-02-17 12:13:46 +0100

    GSAP + Ikonok

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { gsap, Back } from "gsap";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
 
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(".anim1", { 
+      y: 100,
+      duration: 1,
+      delay: 0.5, 
+      opacity: 0, 
+      ease: Back.easeOut, 
+      /*
+      scrollTrigger: {
+        trigger: ".anim1",
+        start: "top 260px",
+        markers: true
+      }
+      */
+    });
+    gsap.from(".anim2", { 
+      y: 100, 
+      duration: 1, 
+      delay: 1, 
+      opacity: 0, 
+      ease: Back.easeOut
+    
+    });
+  }
+  ngOnInit() {
+    console.log("Home loaded.")
+  }
 }
